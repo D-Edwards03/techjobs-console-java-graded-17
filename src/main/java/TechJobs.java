1bb54aefc942c358f7e49455765606333364c393
Demetrius Edwards <edwardsd03@outlook.com>
2024-11-18 00:14:00 -0600

    Task 1: Implement printJobs()

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+            return;
+        }
 
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("*****");
+            for (Map.Entry<String, String> element : job.entrySet()) {
+                System.out.println(element.getKey() + ": " + element.getValue());
+            }
+            System.out.println("*****\n");
+        }
     }
 }
